784f2968084a2549c2ead6fd7d6345e98741b4d8
Dzhangir Rakhmatullin <developer4.lingplay@gmail.com>
2016-08-10 14:38:41 +0500

    Скрипты зданий

---
+++ Assets/scripts/Barraks.cs
@@ -0,0 +1,49 @@
+﻿/// <summary>
+/// Казармы
+/// </summary>
+public class Barraks {
+
+	public enum UnitType {
+		// Воин ближнего боя
+		Warrior,
+		// Воин дальнего боя
+		Archer
+	}
+
+	private UnitType type;
+	private float trainingSpeed;
+	private int level;
+
+	/// <summary>
+	/// Скорость тренировки - юнитов в секунду
+	/// </summary>
+	public float TrainingSpeed {
+		get {
+			return trainingSpeed;
+		}
+		set {
+			trainingSpeed = value;
+		}
+	}
+
+	/// <summary>
+	/// Уровень казармы
+	/// </summary>
+	public int Level {
+		get {
+			return level;
+		}
+		set {
+			level = value;
+		}
+	}
+
+	public UnitType Type {
+		get {
+			return type;
+		}
+		set {
+			type = value;
+		}
+	}
+}
---
+++ Assets/scripts/Divan.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Диван
+/// </summary>
+public class Divan {
+	private int hp = 100; 
+
+	/// <summary>
+	/// Количество жизней
+	/// </summary>
+	public int Hp {
+		get {
+			return hp;
+		}
+		set {
+			hp = value;
+		}
+	}
+}
